a47f9596ae25f6f88bc50da12ddfc991e4ae8f34
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-16 15:41:53 +0530

    js in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,20 @@
 let textbox = document.querySelector('#enter-choice'),
-    choice_tags = document.querySelector('#choice-tags');
+    choice_tags = document.querySelector('#choice-tags'),
+    tag_name = document.querySelector('textarea[]').value,
+    text_pattern = /,$/;
+    
 
 textbox.addEventListener('keypress',function(){
-    console.log("typing");
+  console.log(tag_name);
+  
+  
+  
+    // let tag=document.createElement('span'),
+    //     tag_text =document.createTextNode(tag_name);
+    // tag.setAttribute('class','span-tag');
+    // choice_tags.appendChild(tag);
+    // document.querySelector('.')
+    
 })
 
 
