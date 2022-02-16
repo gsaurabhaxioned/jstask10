90f2e0e7ff3e01440b6346e7a0c26625eb15e6b8
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-16 20:43:21 +0530

    js in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,75 +1,33 @@
 let textbox = document.querySelector('#enter-choice'),
     choice_tags = document.querySelector('#choice-tags'),
-    text_pattern = /,$/,
     text = "";
 
+textbox.addEventListener('keyup', function (e) {
 
-    textbox.addEventListener('keyup',function(e){
-       
-     text = textbox.value;
-     let text_array=text.split(',');
-     if(text.length == 1 || e.keyCode==188){
-         if(e.keyCode == 32){
-        
-     }else{
-        let tag=document.createElement('span');
-             choice_tags.appendChild(tag);
-             tag.setAttribute('class','span-tag');
-     }
+    text = textbox.value;
+    if (text == "") {
+        choice_tags.innerHTML = "";
+    }else{
+    let text_array = text.split(',');
+    if ((text.length == 1 || e.keyCode == 188) && e.keyCode != 32 && e.keyCode != 8) {
+
+        let tag = document.createElement('span');
+        choice_tags.appendChild(tag);
+        tag.setAttribute('class', 'span-tag');
     }
-     let span_tag = document.querySelectorAll('.span-tag');
 
-     text_array.forEach((i,index)=>{
-         span_tag[index].innerText = text_array[index];
-         if(text_array[index]==""){
-             span_tag[index].style.display = "none";
-         }else{
-            span_tag[index].style.display = "inline-block";
-         }
-        
-        });
-    
+    let span_tag = document.querySelectorAll('.span-tag');
+
+    text_array.forEach((i, index) => {
+        span_tag[index].innerText = text_array[index];
     });
-    //e.keyCode==188
-       
 
-    //  text_array.forEach(i=>{
-    //     let tag=document.createElement('span');
-    //     choice_tags.appendChild(tag);
-    //     tag.setAttribute('class','span-tag');
-    //  });
-     
-
-
-
-
-
-
-
-    // if(text.match(text_pattern)){
-    // let tag=document.createElement('span');
-    // choice_tags.appendChild(tag);
-    // tag.setAttribute('class','span-tag');
-    // let span_tag = document.querySelectorAll('.span-tag'),
-    //     span_length = span_tag.length-1;
-    //     span_tag[span_length].innerText = text;
-        // if(e.keyCode === 188){
-        //     console.log("comma");
-        //     text = "";
-        // }
-       
-// }    
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-
+    span_tag.forEach(i => {
+        if (i.innerText == "") {
+            i.style.display = "none";
+        } else {
+            i.style.display = "inline-block";
+        }
+    })
+    }
+});
