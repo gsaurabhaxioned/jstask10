e20022a98f89d9a4839a1b1c0242a0ea171d5e6b
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-16 14:21:56 +0530

    js code started

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,9 @@
-/* Author: 
+let textbox = document.querySelector('#enter-choice'),
+    choice_tags = document.querySelector('#choice-tags');
 
-*/
-
-
-
-
-
-
-
-
-
-
+textbox.addEventListener('keypress',function(){
+    console.log("typing");
+})
 
 
 
